ee8ab380973a9fae34a96cf195286c8709907269
PatosZapatos <patriciozapata1410@gmail.com>
2023-08-23 18:20:04 -0300

    ahora el registro funciona

--- bootstrap/register/register-process.php
+++ bootstrap/register/register-process.php
@@ -1,15 +1,35 @@
 <?php 
+include 'register.php';
+?>
 
+<?php 
 $mail = $_POST['email'];
 $usn = $_POST['username'];
 $psw = $_POST['passw'];
 
+
 $base = "florvax";
+
 $Conexion =  mysqli_connect("localhost","root","",$base);
+$emailcheck = "SELECT * FROM usuario WHERE email='$mail'";
+$usercheck = "SELECT * FROM usuario WHERE username='$usn'";
 
-$cadena= "INSERT INTO usuario(email, username, passw) VALUES ('$mail','$usn','$psw')";
+$check_email_for_duplicates = mysqli_query($Conexion,$emailcheck);
+$check_user_for_duplicates = mysqli_query($Conexion,$usercheck);
 
-$resultado = mysqli_query($Conexion,$cadena);
+if(mysqli_num_rows($check_email_for_duplicates) > 0 || mysqli_num_rows($check_user_for_duplicates) > 0) {
+    echo "<script> 
+    document.getElementById('wrong_pass_alert').style.color = 'red';
+    document.getElementById('wrong_pass_alert').innerHTML ='☒ El nombre de usuario o mail ya fue registrado, ingrese otro.'; 
+    </script>";
+}
+else {
+    
+    $cadena= "INSERT INTO usuario(email, username, passw) VALUES ('$mail','$usn','$psw')";
+    
+    $resultado = mysqli_query($Conexion,$cadena);
+    
+    echo "<script>location.href='../../paginaprincipal/pag1.html'</script>";
+}
 
-echo "<script>location.href='../../paginaprincipal/pag1.html'</script>"
- ?>
+?>
--- bootstrap/register/register.php
+++ bootstrap/register/register.php
@@ -169,36 +169,7 @@
   </main>
   <script src="../js/bootstrap.bundle.js"></script>
 
-  <script>
-    function validate_password() {
-
-      var pass = document.getElementById('password1').value;
-      var confirm_pass = document.getElementById('password2').value;
-      if (pass != confirm_pass) {
-        document.getElementById('wrong_pass_alert').style.color = 'red';
-        document.getElementById('wrong_pass_alert').innerHTML
-          = '☒ Las contraseñas deben coincidir.';
-        document.getElementById('create').disabled = true;
-        document.getElementById('create').style.opacity = (0.4);
-      } else {
-        document.getElementById('wrong_pass_alert').style.color = 'green';
-        document.getElementById('wrong_pass_alert').innerHTML =
-          '🗹 Las contraseñas coinciden.';
-        document.getElementById('create').disabled = false;
-        document.getElementById('create').style.opacity = (1);
-      }
-    }
-
-    function wrong_pass_alert() {
-      if (document.getElementById('password1').value != "" &&
-        document.getElementById('confirm_pass').value != "") {
-        alert("Your response is submitted");
-      } else {
-        alert("Please fill all the fields");
-      }
-    }
-
-  </script>
+  <script src="register.js"></script>
 
   <script>
     const togglePassword1 = document.querySelector("#togglePassword1");
