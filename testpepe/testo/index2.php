2f558b77149067e20d2dae2760408bac3ec38152
danteuri <santiagomaytahuanca@gmail.com>
2023-09-05 22:54:03 -0300

    k
    k

--- testpepe/testo/index2.php
+++ testpepe/testo/index2.php
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Florvax</title>
     <link rel="stylesheet" href="../../paginaprincipal/estilos.css">
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="../../paginaprincipal/imagenes/logo_florvax.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
 </head>
+<body>
+    <header>
+		<h1>Florvax</h1>
+		<nav>
+			<ul>
+                <li><a href="#">Pricing</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Pages</a></li>
+				<li><a href="#">Help</a></li>
+				<li><a href="../../bootstrap/register/register.php">Sign Up</a></li>
+				<li><a href="../../bootstrap/sign-in/sign-in.html">Log In</a></li>
+			</ul>
+		</nav>
+    </header>
+	<section class="zona1"></section>
+   
+	<script type="text/javascript">
+		window.addEventListener("scroll", function(){
+			var header = document.querySelector("header");
+			header.classList.toggle("abajo",window.scrollY>0);
+		})
+	</script>
+        <div class="uno">
 
-<?php
-    include '../../paginaprincipal/pag1.php'
-    
-?>
+			<div class="text1">
+				<h2> powerful tagline for your product </h2>
+				<p>supporting statement for your produc is tagline. this text should communicate your unique selling propositin to the reader</p>
+			</div>
+		</div>
+</body>
+</html>
