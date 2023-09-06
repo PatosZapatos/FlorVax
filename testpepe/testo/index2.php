f7a2e77a41d74d36bd6e29f556554c8189a5151d
danteuri <santiagomaytahuanca@gmail.com>
2023-09-05 23:24:07 -0300

    r
    r

--- testpepe/testo/index2.php
+++ testpepe/testo/index2.php
@@ -5,23 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Florvax</title>
-    <link rel="stylesheet" href="../../paginaprincipal/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 
 <body>
-    <header>
-        <h1>Florvax</h1>
-        <nav>
-            <ul>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Pages</a></li>
-                <li><a href="#">Help</a></li>
-                <li><a href="../../bootstrap/register/register.php">Sign Up</a></li>
-                <li><a href="../../bootstrap/sign-in/sign-in.html">Log In</a></li>
-            </ul>
-        </nav>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            <a href="../../paginaprincipal/imagenes/logo_florvax.png" class="d-inline-flex link-body-emphasis text-decoration-none">
+                <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+            </a>
+        </div>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2">Features</a></li>
+            <li><a href="#" class="nav-link px-2">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2">About</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
     </header>
     <section class="zona1"></section>
 
@@ -31,8 +37,11 @@
             header.classList.toggle("abajo", window.scrollY > 0);
         })
     </script>
-    <div class="container">
-        <input type="text">
+
+    <div>
+        <div class="container">
+            <input type="text">
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
