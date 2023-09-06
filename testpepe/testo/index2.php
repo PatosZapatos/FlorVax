e26296024ee4ec0b5f751d213ab5d2ff9cd4be16
danteuri <santiagomaytahuanca@gmail.com>
2023-09-05 23:14:23 -0300

    editando

--- testpepe/testo/index2.php
+++ testpepe/testo/index2.php
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Florvax</title>
     <link rel="stylesheet" href="../../paginaprincipal/estilos.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
+
 <body>
     <header>
-		<h1>Florvax</h1>
-		<nav>
-			<ul>
+        <h1>Florvax</h1>
+        <nav>
+            <ul>
                 <li><a href="#">Pricing</a></li>
-				<li><a href="#">Blog</a></li>
-				<li><a href="#">Pages</a></li>
-				<li><a href="#">Help</a></li>
-				<li><a href="../../bootstrap/register/register.php">Sign Up</a></li>
-				<li><a href="../../bootstrap/sign-in/sign-in.html">Log In</a></li>
-			</ul>
-		</nav>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="../../bootstrap/register/register.php">Sign Up</a></li>
+                <li><a href="../../bootstrap/sign-in/sign-in.html">Log In</a></li>
+            </ul>
+        </nav>
     </header>
-	<section class="zona1"></section>
-   
-	<script type="text/javascript">
-		window.addEventListener("scroll", function(){
-			var header = document.querySelector("header");
-			header.classList.toggle("abajo",window.scrollY>0);
-		})
-	</script>
-        <div class="uno">
+    <section class="zona1"></section>
 
-			<div class="text1">
-				<h2> powerful tagline for your product </h2>
-				<p>supporting statement for your produc is tagline. this text should communicate your unique selling propositin to the reader</p>
-			</div>
-		</div>
-</body>
-</html>
+    <script type="text/javascript">
+        window.addEventListener("scroll", function() {
+            var header = document.querySelector("header");
+            header.classList.toggle("abajo", window.scrollY > 0);
+        })
+    </script>
+    <div class="container">
+        <input type="text">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+    <body>
