c3e11e2d15f1216f26d08b64759b3f4256b71c76
tobiane <42877632+tobiane@users.noreply.github.com>
2023-09-27 17:44:01 -0300

    todo

---
+++ tobi/compra/lechuga.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlorVax</title>
+    <link rel="stylesheet" href="../css/compra.css">
+</head>
+<body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
+    <menu>
+    FlorVax 
+
+    </menu>
+</body>
+</html>
