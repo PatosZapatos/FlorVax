8fccccc990a4feb63eb8bdb5bc0d8fee652b8e0e
tobiane <42877632+tobiane@users.noreply.github.com>
2023-09-27 17:59:38 -0300

    poco a poco

--- tobi/compra/lechuga.php
+++ tobi/compra/lechuga.php
@@ -7,9 +7,29 @@
     <link rel="stylesheet" href="../css/compra.css">
 </head>
 <body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
-    <menu>
-    FlorVax 
-
-    </menu>
+    <head>
+        <div class="header">
+        <h2 class="flor">FlorVax</h2>
+        <div class="input">
+        <input id="buscar" placeholder="Buscar" class="buscar" style="width: 500px; height: 30px;">
+        <button id="btn" class="btn" style="width: 100px; height: 40px;">Buscar🔎</button>
+        </div>
+        
+        <div class="container">   
+                        <ul>
+                            <li class="dropdown">
+                                <a>Categoria</a>
+                                <div class="dropdown-content">
+                                    <a href="../forms/semillas.php">Semillas</a>
+                                    <a href="../forms/herrramientas.php">Herramientas</a>
+                                </div>
+                                </li>
+                            <li><a href="">Carrito</a></li>
+                            <li><a href="">Usuario</a></li>
+        </div>
+        
+            
+        </div>
+    </head>
 </body>
 </html>
