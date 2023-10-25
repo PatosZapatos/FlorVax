11a67d089142ca74c0deb17eda9beddef1ef4afd
danteuri <santiagomaytahuanca@gmail.com>
2023-10-25 17:54:38 -0300

    danteuri updates
    afdvswdvfsd soy gay

--- florvax (1).sql
+++ florvax (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-10-2023 a las 22:19:18
+-- Tiempo de generación: 25-10-2023 a las 22:53:46
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
