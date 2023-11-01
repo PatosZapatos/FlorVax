bb36a992179353a277b40d02e60510912b4ce73f
PatosZapatos <patriciozapata1410@gmail.com>
2023-11-01 14:54:32 -0300

    Boton Tienda Activado

--- bootstrap/testpepitos/pepes.php
+++ bootstrap/testpepitos/pepes.php
@@ -26,7 +26,7 @@ $Conexion = mysqli_connect("localhost", "root", "", $base);
         </div>
 
         <ul class="nav col mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-white fw-bold">Tienda</a></li>
+          <li><a href="/php/florvax/tobi/forms/herramientasmejor.php" class="nav-link px-2 text-white fw-bold">Tienda</a></li>
           <li><a href="#" class="nav-link px-2 text-white fw-bold mx-4">Donaciones</a></li>
           <li><a href="/php/florvax/tobi/forms/nosotrosmejor.php" class="nav-link px-2 text-white fw-bold">Acerca de
               Nosotros</a></li>
@@ -42,7 +42,7 @@ $Conexion = mysqli_connect("localhost", "root", "", $base);
 
             $consulta = mysqli_query($Conexion, $cadena);
             $registro = mysqli_fetch_row($consulta);
-            
+
             if ($registro[4] != "") {
               echo "<img class= 'center-image' src='data:image/jpeg;base64," . base64_encode($registro[4]) . "' width='32px'/>";
             } else {
@@ -54,6 +54,18 @@ $Conexion = mysqli_connect("localhost", "root", "", $base);
               <a href="/php/florvax/bootstrap/register/register.php"><button type="button" class="btn btn-outline-light">Registrate</button></a>';
           }
           ?>
+        </div>
+        <div class="flex-shrink-0 dropdown">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small shadow">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
         </div>
       </header>
     </div>
