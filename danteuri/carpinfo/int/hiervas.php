65992ee20384a65b87ebe6830b4b2e4367f31f3d
AlanHuanca21353 <santiagomaytahuanca@gmail.com>
2023-11-15 16:05:24 -0300

    coso
    a

--- danteuri/carpinfo/int/hiervas.php
+++ danteuri/carpinfo/int/hiervas.php
@@ -2,32 +2,43 @@
 // Este Include llama al Header, sin importar donde el Archivo actual se encuentre
 include $_SERVER['DOCUMENT_ROOT'] . '/php/florvax/bootstrap/testpepitos/pepes.php';
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Florvax</title>
     <link rel="stylesheet" href="style2.css">
 </head>
+
 <body>
-<h1 class="tittle">
-        Informacion de las Hiervas
+	<h1 class="tittle">
+        Informacion de la Flora
     </h1>
 
     <div class="secciones">
-        <a href="arboles.php">
+        <a href="int/arboles.php">
             <img src="../img/ejarbol.jpg" alt="">
         </a>
-        <a href="plantas.php">
+        <a href="int/plantas.php">
             <img src="../img/ejplanta.jpg" alt="">
         </a>
-        <a href="frutas.php">
+        <a href="int/frutas.php">
             <img src="../img/ejfruta.jpg" alt="">
         </a>
-        <a href="hiervas.php">
+        <a href="int/hiervas.php">
             <img src="../img/ejhierva.jpg" alt="">
         </a>
+        <a href="int/verduras.php">
+            <img src="../img/verdura.jpg" alt="">
+        </a>
     </div>
+
 </body>
+
 </html>
+<?php
+    include 'coso.php'
+?>
