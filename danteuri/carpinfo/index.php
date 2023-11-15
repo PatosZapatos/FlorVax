0a8321ecc4b777b393975264cc8b6792ca92f515
danteuri <santiagomaytahuanca@gmail.com>
2023-11-14 21:25:28 -0300

    pagina de informacion 1.05.0a1

--- danteuri/carpinfo/index.php
+++ danteuri/carpinfo/index.php
@@ -10,11 +10,14 @@ include $_SERVER['DOCUMENT_ROOT'] . '/php/florvax/bootstrap/testpepitos/pepes.ph
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Florvax</title>
-    <link rel="stylesheet" href = "estilos.css" >
+    <link rel="stylesheet" href="style2.css">
 </head>
 
 <body>
-	
+	<h1 class="tittle">
+        Informacion de la Flora
+    </h1>
+
 </body>
 
 </html>
